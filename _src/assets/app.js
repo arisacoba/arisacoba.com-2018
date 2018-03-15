6ce7f2ff72519e19f5d81c4bbb6ad68740512662
Aris Acoba <aris.acoba@gmail.com>
2018-03-15 10:19:18 +0800

    working stacking. index, previous and next siblings are okay

--- _src/assets/app.js
+++ _src/assets/app.js
@@ -1,45 +1,36 @@
 function getCurrentClass(obj_name){
     return obj_name.className.match(/-s\d+/) + '';
 }
-function getNextClass(obj_name){
-    if (obj_name.nextSibling !== null){
-        return obj_name.nextSibling.className.match(/-s\d+/) + '';
-    }
-    return '';
-}
-function getPrevClass(obj_name){
-    if (obj_name.previousSibling !== null){
-        return obj_name.previousSibling.className.match(/-s\d+/) + '';
-    }
-    return '';
-}
 
 $(".center").on("click", ".stack", function() {
     var _begin = '-s0';
     var _end = '-s6';
     var _numOfChildren = $('.center').children().length;
-    console.log( 'numOfChildren: ' + _numOfChildren );
 
     var _current = this;
     var _currentIndex = $(this).index();
     var _currentClass = getCurrentClass(this);
 
-
     console.log( '--- index: ' +  _currentIndex + ' current: ' + _currentClass );
-
-    //$(this).removeClass($currentClass).addClass( $begin )
-
-    $(_current).removeClass( getCurrentClass(_current) ).addClass( _begin );
 
     var _next = _current.nextSibling;
     var _prev = _current.previousSibling;
     var _nextIndex = 0;
     var _prevIndex = _currentIndex;
+    var _inCount = _numOfChildren;
+
+    if ( getCurrentClass(_current) === _begin ){
+        $(_current).removeClass( getCurrentClass(_current) ).addClass( _end);
+        var _nextIndex = -1;
+        _inCount -= 1;
+    }else{
+        $(_current).removeClass( getCurrentClass(_current) ).addClass( _begin );
+    }
 
     // loop on next siblings
     if ( _next !== null ){
         while( _next !== null ){
-            var _nextIndex = _nextIndex + 1 ;
+            _nextIndex += 1 ;
             $(_next).removeClass( getCurrentClass(_next) ).addClass( '-s' + _nextIndex );
             var _next = _next.nextSibling;
         }
@@ -47,14 +38,12 @@ $(".center").on("click", ".stack", function() {
 
     // loop on previous siblings
     if ( _currentIndex > 0 ){
-        var _inCount = _numOfChildren;
-
         while( _currentIndex !== 0 ){
             var _prevIndex = _inCount - 1;
             $(_prev).removeClass( getCurrentClass(_prev) ).addClass( '-s' + _prevIndex );
             var _prev = _prev.previousSibling;
-            var _inCount = _inCount - 1;
-            var _currentIndex = _currentIndex - 1;
+            _inCount -= 1;
+            _currentIndex -= 1;
         }
 
     }
